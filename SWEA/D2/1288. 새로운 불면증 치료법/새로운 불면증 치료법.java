2258b682aa0f796cd617e5fa6d57060b46f2f33a
Kim Areum <101612927+aa001R@users.noreply.github.com>
2024-06-22 00:51:55 +0900

    [D2] Title: 새로운 불면증 치료법, Time: 113 ms, Memory: 18,792 KB -BaekjoonHub

--- SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.java
+++ SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.java
@@ -1,33 +1,26 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+#include <iostream>
+#include <string>
+using namespace std;
 
-public class Solution {
-	public static void main(String[] args) throws IOException{		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int T = Integer.parseInt(br.readLine());
-		for(int testcase = 1; testcase <= T; testcase++) {
-			int n = Integer.parseInt(br.readLine());
-			int cnt = 0;
-			int check = 1 << 10;
-			
-			while((check & ((1 << 10) - 1)) != ((1 << 10) - 1)) {
-				cnt++;
-				int order = n * cnt;
-				while(order > 0) {
-					check |= (1 << (order % 10));
-					order /= 10;
-				}
-			}
-			bw.append("#").append(Integer.toString(testcase)).append(" ")
-			.append(Integer.toString(cnt * n));
-			bw.newLine();
-		}
-		bw.flush();
-	}
+int main() {
+    int testcase;
+    cin >> testcase;
+    int total = (1 << 10) - 1;  // 모든 숫자가 등장했을 때의 값
 
+    for (int i = 1; i <= testcase; i++) {
+        int N;
+        cin >> N;
+        int visited = 0;
+        int count = 0;
+        while (true) {
+            string strNum = to_string(N * (++count));  // N*count 값을 문자열로 표현한 것  (예: 5 * 13 = 65 -> "65")
+            for (char c : strNum) {
+                int num = c - '0';
+                visited |= (1 << num);  // 각 숫자에 대해 등장했다는 의미로 bit 를 1로 변경
+            }
+            if (visited == total) break;  // 모든 숫자가 등장했다면, 종료
+        }
+        cout << "#" << i << " " << N * count << endl;
+    }
+    return 0;
 }
