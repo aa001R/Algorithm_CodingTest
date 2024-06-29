3b3150044f644f801f0c4394754a4f9d60e9dd27
Kim Areum <101612927+aa001R@users.noreply.github.com>
2024-06-30 02:29:12 +0900

    [Gold V] Title: 수 나누기 게임, Time: 336 ms, Memory: 35532 KB -BaekjoonHub

--- 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.java
+++ 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.java
@@ -2,42 +2,28 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-	static class Node{
-		Node next;
-		int num;
-		public Node(int num, Node next) {
-			this.num = num;
-			this.next = next;
-		}
-	}
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	static int N, score[] = new int[1_000_001], player[];
+	static int N, score[], player[], min = 1_000_001, max = 0;
 	static boolean card[] = new boolean[1_000_001];
-	static ArrayList<Integer> [] divisor;
 	public static void main(String[] args) throws IOException
 	{
 		N = Integer.parseInt(br.readLine());
 		player = new int[N];
-		divisor = new ArrayList[N];
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < N; i++) {
 			player[i] = Integer.parseInt(st.nextToken());
 			card[player[i]] = true;
-			divisor[i] = new ArrayList<>();
+			if(max < player[i]) max = player[i];
 		}
-		for(int i = 0; i < N; i++) {
-			for(int j = 1; j * j <= player[i]; j++) {
-				if(player[i] % j == 0 ) divisor[i].add(j);
-				if(j*j != player[i] && player[i] % j == 0) divisor[i].add(player[i] / j);
-			}
-		}
-		for(int i = 0; i < N; i++) {
-			for(int d : divisor[i]) {
-				if(!card[d]) continue;
-				score[d]++; score[player[i]]--;
-			}
-		}
+		score = new int[max+1];
+		for (int i = 0; i < N; i++) {
+            for (int j = player[i]*2; j <= max; j+=player[i]) {
+                if (!card[j]) continue;
+                score[player[i]]++; score[j]--;
+            }
+        }
+		
 		for(int i = 0; i < N; i++) {
 			bw.append(Integer.toString(score[player[i]])).append(" ");
 		}
