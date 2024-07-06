ef26b841c50ab5c0bd3576bd86a206354a0dada9
Kim Areum <101612927+aa001R@users.noreply.github.com>
2024-07-07 00:42:42 +0900

    [Silver II] Title: 내 뒤에 나와 다른 수, Time: 660 ms, Memory: 72580 KB -BaekjoonHub

--- 백준/Silver/24523. 내 뒤에 나와 다른 수/내 뒤에 나와 다른 수.java
+++ 백준/Silver/24523. 내 뒤에 나와 다른 수/내 뒤에 나와 다른 수.java
@@ -3,15 +3,14 @@ import java.io.*;
 
 class Main {
 	static int N, arrNum[], diffIdx[];
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		N = Integer.parseInt(br.readLine());
+		N = read();
 		arrNum = new int[N];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int now = Integer.parseInt(st.nextToken());
+		int now = read();
 		for(int i = 1; i < N; i++) {
-			int cur = Integer.parseInt(st.nextToken());
+			int cur = read(); // 현재 숫자
 			if(now != cur) {
 				now = cur;
 				arrNum[i] = arrNum[i-1] + 1; 
@@ -39,4 +38,26 @@ class Main {
 		if(end == N) return -1;
 		return end+1;
 	}
+	
+	public static int read() throws Exception{
+        int n = 0;
+        int cur;
+        boolean isNumber = false;
+        boolean isNegative = false;
+        while(true){
+            cur = System.in.read();
+            if(cur == '-'){
+                isNegative = true;
+            }
+            else if(cur <= 32){
+                if(isNumber){ return isNegative ? -n : n; }
+            }
+            else{
+                isNumber = true;
+                n = (n<<3) + (n<<1) + (cur&15);
+                // n의 10배(8배+2배) + 새로운 숫자(c&15)로 n = 10 * n + c - 48 연산과 동일
+                // 0 -> 아스키코드 48 110000~111111에서 110000을 제외한 0000~1111값만을 확인
+            }
+        }
+    }
 }
