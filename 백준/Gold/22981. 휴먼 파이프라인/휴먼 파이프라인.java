7bdc4b6e914709f0e7dcddf2386a3520c683970d
Kim Areum <101612927+aa001R@users.noreply.github.com>
2024-07-16 22:48:14 +0900

    [Gold V] Title: 휴먼 파이프라인, Time: 588 ms, Memory: 56248 KB -BaekjoonHub

---
+++ 백준/Gold/22981. 휴먼 파이프라인/휴먼 파이프라인.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		long K = Long.parseLong(st.nextToken());
+		
+		ArrayList <Long> workPerMinute = new ArrayList<>();
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			workPerMinute.add(Long.parseLong(st.nextToken()));
+		}
+		Collections.sort(workPerMinute);
+		
+		long maxTeamWork = 0;
+		for(int i = 0; i < N-1; i++) {
+			long teamWork = workPerMinute.get(0) * (i+1) + workPerMinute.get(i+1) * (N - (i+1)); 
+			if(teamWork <= maxTeamWork) continue;
+			maxTeamWork = teamWork;
+		}
+		long workTime = K % maxTeamWork == 0 ? K / maxTeamWork : K / maxTeamWork + 1;
+		bw.append(Long.toString(workTime));
+		bw.flush();
+	}
+}
