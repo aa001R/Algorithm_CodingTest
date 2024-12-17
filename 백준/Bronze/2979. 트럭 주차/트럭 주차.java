4d0713f22d750121e664689a67ae11d39fb1e9c3
Kim Areum <101612927+aa001R@users.noreply.github.com>
2024-12-18 00:18:07 +0900

    [Bronze II] Title: 트럭 주차, Time: 64 ms, Memory: 11348 KB -BaekjoonHub

--- 백준/Bronze/2979. 트럭 주차/트럭 주차.java
+++ 백준/Bronze/2979. 트럭 주차/트럭 주차.java
@@ -1,45 +1,32 @@
 import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		int A = read();
 		int B = read();
 		int C = read();
-		List<ParkingTime> parkingTimes = new ArrayList<>();
-		int minT = 101, maxT = 0;
+		int [] time = new int [101];
 		for (int i = 0; i < 3; i++) {
 			int startT = read();
 			int endT = read();
-			parkingTimes.add(new ParkingTime(startT, endT));
-			if (startT < minT) { minT = startT; }
-			if (endT > maxT) { maxT = endT; }
-		}
-
-		int [] time = new int [101];
-		for (ParkingTime parkingTime : parkingTimes) {
-			for (int j = parkingTime.startT; j < parkingTime.endT; j++) {
+			for (int j = startT; j < endT; j++) {
 				time[j]++;
 			}
 		}
 
 		int cost = 0;
-		for (int i = minT; i <= maxT; i++) {
-			switch (time[i]){
+		for (int t : time) {
+			switch (t){
 				case 1 : cost += A; break;
 				case 2 : cost += (B*2); break;
 				case 3 : cost += (C*3); break;
 			}
 		}
 		System.out.println(cost);
-	}
-
-	static class  ParkingTime {
-		int startT, endT;
-		public ParkingTime(int startT, int endT) {
-			this.startT = startT;
-			this.endT = endT;
-		}
 	}
 
 	public static int read() throws IOException {
