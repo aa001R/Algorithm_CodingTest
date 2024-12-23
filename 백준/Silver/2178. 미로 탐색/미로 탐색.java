ff6a4c4545df75515c78e283f5bc6753700ceea9
Kim Areum <101612927+aa001R@users.noreply.github.com>
2024-12-23 22:29:59 +0900

    [Silver I] Title: 미로 탐색, Time: 80 ms, Memory: 12092 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,52 +1,48 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 	static int N;
 	static int M;
-	static String [] map;
+	static String[] map;
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		map = new String[N];
-		for(int i = 0; i < N; i++) {
+		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
 			map[i] = st.nextToken();
 		}
 		System.out.println(bfs(0, 0));
 	}
-	
+
 	static int bfs(int r, int c) {
 		Queue<int[]> que = new ArrayDeque<>();
-		int [][] visit = new int[N][M];
-		int [][] delta = {{1, 0},{-1, 0},{0, -1},{0, 1}};
-		
+		int[][] visit = new int[N][M];
+		int[][] delta = {{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
+
 		que.offer(new int[] {r, c});
 		visit[r][c] = 1;
-		
-		while(!que.isEmpty()) {
-			int [] n = que.poll();
-			for(int i = 0; i < 4; i++) {
+
+		while (!que.isEmpty()) {
+			int[] n = que.poll();
+			for (int i = 0; i < 4; i++) {
 				int nextr = n[0] + delta[i][0];
 				int nextc = n[1] + delta[i][1];
-				if(isIn(nextr, nextc) && visit[nextr][nextc] == 0 && map[nextr].charAt(nextc) == '1') {
+				if (isIn(nextr, nextc) && visit[nextr][nextc] == 0 && map[nextr].charAt(nextc) == '1') {
 					visit[nextr][nextc] = visit[n[0]][n[1]] + 1;
-					que.offer(new int[]{nextr, nextc});
+					que.offer(new int[] {nextr, nextc});
 				}
 			}
 		}
-		
-		return visit[N-1][M-1];
+
+		return visit[N - 1][M - 1];
 	}
-	
+
 	static boolean isIn(int r, int c) {
-		return (r >= 0 && r < N && c>=0 && c < M);
+		return (r >= 0 && r < N && c >= 0 && c < M);
 	}
 }
