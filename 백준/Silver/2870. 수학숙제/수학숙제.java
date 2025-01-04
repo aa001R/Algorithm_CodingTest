7d694837fdb7d22284e0a0de8ca5dbd40ee87a5b
Kim Areum <101612927+aa001R@users.noreply.github.com>
2025-01-04 18:54:07 +0900

    [Silver IV] Title: 수학숙제, Time: 88 ms, Memory: 12960 KB -BaekjoonHub

---
+++ 백준/Silver/2870. 수학숙제/수학숙제.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+import java.util.regex.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        List<BigInteger> numbers = new ArrayList<>(); // BigInteger 사용
+        int n = Integer.parseInt(br.readLine());
+        Pattern pattern = Pattern.compile("\\d+");
+
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            Matcher matcher = pattern.matcher(s);
+            while (matcher.find()) {
+                numbers.add(new BigInteger(matcher.group()));
+            }
+        }
+
+        Collections.sort(numbers);
+
+        for (BigInteger number : numbers) {
+            bw.append(number.toString()).append("\n");
+        }
+        bw.flush();
+    }
+}
