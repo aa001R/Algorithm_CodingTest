d5879681f1efac347f88e1188291679aa30dbe28
Kim Areum <101612927+aa001R@users.noreply.github.com>
2025-01-10 22:14:21 +0900

    [Bronze III] Title: 골뱅이 찍기 - 돌아간 ㄹ, Time: 116 ms, Memory: 15132 KB -BaekjoonHub

--- 백준/Bronze/23805. 골뱅이 찍기 － 돌아간 ㄹ/골뱅이 찍기 － 돌아간 ㄹ.java
+++ 백준/Bronze/23805. 골뱅이 찍기 － 돌아간 ㄹ/골뱅이 찍기 － 돌아간 ㄹ.java
@@ -1,66 +1,19 @@
 import java.io.*;
-import java.lang.reflect.Array;
-import java.util.Arrays;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int N = read();
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				for (int k = 0; k < 3; k++) {
-					bw.append("@");
-				}
-			}
-			for (int j = 0; j < N; j++) {
-				bw.append(" ");
-			}
-			for (int j = 0; j < N; j++) {
-				bw.append("@");
-			}
-			bw.newLine();
-		}
-		for (int k = 0; k < 3; k++) {
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < N; j++) {
-					bw.append("@");
-				}
-				for (int j = 0; j < N; j++) {
-					bw.append(" ");
-				}
-				for (int j = 0; j < N; j++) {
-					bw.append("@");
-				}
-				for (int j = 0; j < N; j++) {
-					bw.append(" ");
-				}
-				for (int j = 0; j < N; j++) {
-					bw.append("@");
-				}
-				bw.newLine();
-			}
-		}
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				bw.append("@");
-			}
-			for (int j = 0; j < N; j++) {
-				bw.append(" ");
-			}
-			for (int j = 0; j < N; j++) {
-				for (int k = 0; k < 3; k++) {
-					bw.append("@");
-				}
-			}
-			bw.newLine();
-		}
+		bw.append(String.format("%s%s%s\n", "@".repeat(3 * N), " ".repeat(N), "@".repeat(N)).repeat(N));
+		bw.append(String.format("%s%s%s%s%s\n", "@".repeat(N)," ".repeat(N),"@".repeat(N)," ".repeat(N), "@".repeat(N)).repeat(3 * N));
+		bw.append(String.format("%s%s%s\n" ,"@".repeat(N), " ".repeat(N), "@".repeat(3 * N)).repeat(N));
 		bw.flush();
 	}
 
-	static int read() throws Exception{
-		int n = System.in.read() & 15, cur;
+	static int read() throws Exception {
+		int n = 0, cur;
 		while ((cur = System.in.read()) > 32) {
-			n = (n << 3) + (n << 1) + (cur & 15);
+			n = (n * 10) + (cur - '0');
 		}
 		return n;
 	}
