2af0fb3f5bbe1595838fc5a313209e85f356d1cd
Kim Areum <101612927+aa001R@users.noreply.github.com>
2025-01-10 21:06:35 +0900

    [Gold III] Title: 사다리 조작, Time: 68 ms, Memory: 13372 KB -BaekjoonHub

--- 백준/Gold/15684. 사다리 조작/사다리 조작.java
+++ 백준/Gold/15684. 사다리 조작/사다리 조작.java
@@ -11,14 +11,18 @@ public class Main {
 			int b = read();
 			ladder[a][b] = true;
 		}
-		bw.write(Integer.toString(addLadder(N, H, M, ladder)));
+		bw.write(Integer.toString(solve(N, H, M, ladder)));
 		bw.flush();
 	}
 
-	private static int addLadder(int n, int h, int m, boolean[][] ladder) {
+	private static int solve(int n, int h, int m, boolean[][] ladder) {
 		if (countOddLines(ladder, n, h) > 3) {
+			//홀수개 가로선이 이미 있는 칸이 3개보다 많으면 각 칸에 +1 가로선을 해야함 => 정답이 3보다 큼
 			return -1;
 		}
+		// 추가할 수 있는 사다리는 최대 3개
+		// 기존 가로선이 홀수 개라면 홀수 개 추가 / 짝수 개면 최대 짝수 개 추가 
+		// 최소 개수이니 작은 값부터 확인 (홀수 : 1 -> 3 / 짝수 : 0 -> 2 순서로 확인)
 		for (int limit = m % 2; limit < 4; limit += 2) {
 			if (tryAddLadders(n, h, ladder, limit)) {
 				return limit;
@@ -32,13 +36,9 @@ public class Main {
 		for (int x = 1; x < n; x++) {
 			boolean isOdd = false;
 			for (int y = 1; y <= h; y++) {
-				if (ladder[y][x]) {
-					isOdd = !isOdd;
-				}
+				if (ladder[y][x]) isOdd = !isOdd;
 			}
-			if (isOdd) {
-				oddCount++;
-			}
+			if (isOdd) oddCount++;
 		}
 		return oddCount;
 	}
@@ -48,23 +48,20 @@ public class Main {
 	}
 
 	private static boolean dfs(int n, int h, int y, int x, int count, int limit, boolean[][] ladder) {
-		if (count == limit) {
-			return validateLadders(n, h, ladder);
-		}
+		if (count == limit) return validateLadders(n, h, ladder);
 
+		// 현재 줄에서 이후로 가로선을 긋기
 		for (int nx = x; nx < n; nx++) {
-			if (addLadderAndCheck(n, h, y, nx, count, limit, ladder)) {
-				return true;
-			}
+			if (addLadderAndCheck(n, h, y, nx, count, limit, ladder)) return true;
 		}
 
+		// 다음 줄에서 처음부터 가로선 긋기
 		for (int ny = y + 1; ny <= h; ny++) {
 			for (int nx = 1; nx < n; nx++) {
-				if (addLadderAndCheck(n, h, ny, nx, count, limit, ladder)) {
-					return true;
-				}
+				if (addLadderAndCheck(n, h, ny, nx, count, limit, ladder)) return true;
 			}
 		}
+		
 		return false;
 	}
 
