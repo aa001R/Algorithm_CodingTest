6e4bfcbdae42076e758b94245e25fd89b4987b47
Kim Areum <101612927+aa001R@users.noreply.github.com>
2025-01-23 01:46:35 +0900

    [Gold I] Title: 동전 뒤집기, Time: 352 ms, Memory: 12328 KB -BaekjoonHub

--- 백준/Gold/1285. 동전 뒤집기/동전 뒤집기.java
+++ 백준/Gold/1285. 동전 뒤집기/동전 뒤집기.java
@@ -1,42 +1,44 @@
 import java.io.*;
 
 public class Main {
-	static int N;
-	static char[][] coins;
+	static int N, min;
+	static int[] coins;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		N = Integer.parseInt(br.readLine());
-		coins = new char[N][N];
+		coins = new int[N];
 		for(int i = 0; i < N; i++) {
-			String input = br.readLine();
+			String input = br.readLine(); int coin = 1;
 			for(int j = 0; j < N; j++) {
-				coins[i][j] = input.charAt(j);
-			}
-		}
-
-		int min = Integer.MAX_VALUE;
-		// bit는 뒤집은 행을 나타내는 부분집합
-		for(int bit = 1; bit < (1 << N); bit++) {
-			int sum = 0;
-			for(int c = 0; c < N; c++) {
-				int back = 0;
-				for(int r = 0; r < N; r++) { // 열을 기준으로 확인함
-					char curr = coins[r][c];
-					if((bit & (1 << r)) != 0) curr = reverse(curr); // 해당 행은 뒤집었음
-					if(curr == 'T') back++;
+				if (input.charAt(j) == 'T') {
+					coins[i] |= coin;
 				}
-				// 행이 뒤집혔든 아니든,
-				// back : 해당 열은 안 뒤집음 / N-back : 해당 열도 뒤집음 => 최소값 경우 선택
-				sum += Math.min(back, N-back);
+				coin *= 2;
 			}
-			min = Math.min(min, sum);
 		}
+		min = Integer.MAX_VALUE;
+		reverse(1);
 		bw.write(Integer.toString(min));
 		bw.flush();
 	}
-	public static char reverse(char curr) {
-		if(curr == 'T') return 'H';
-		return 'T';
+
+	public static void reverse(int index){
+		if(index == N+1){
+			int sum = 0;
+			for (int col = 1; col <= (1 << (N - 1) ) ; col *= 2) {
+				int cnt = 0;
+				for (int row = 0; row < N ; row++) {
+					if((col & coins[row]) != 0 ) cnt++;
+				}
+				sum += Math.min(cnt, N-cnt);
+			}
+			min = Math.min(min, sum);
+			return;
+		}
+
+		reverse(index + 1);
+		coins[index-1] = ~coins[index-1]; // 행 뒤집기
+		reverse(index + 1);
 	}
 }
