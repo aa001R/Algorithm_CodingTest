68e8f3ce33ad2851dba6026f5c67eb358c1d23ce
Kim Areum <101612927+aa001R@users.noreply.github.com>
2025-01-24 20:06:59 +0900

    [Gold IV] Title: 가르침, Time: 96 ms, Memory: 11928 KB -BaekjoonHub

--- 백준/Gold/1062. 가르침/가르침.java
+++ 백준/Gold/1062. 가르침/가르침.java
@@ -4,20 +4,17 @@ import java.io.*;
 public class Main {
 	static int N, K, max;
 	static int[] words;
-	static int learnCnt;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		N = read();
 		K = read();
 		words = new int[N];
-		int charBits = 0;
 		for (int i = 0; i < N; i++) {
 			String word = br.readLine();
 			int len = word.length();
 			for (int j = 4; j < len - 4; j++) {
 				int charIndex = word.charAt(j) - 'a';
-				charBits |= (1 << charIndex);
 				words[i] |= (1 << charIndex);
 			}
 		}
@@ -26,13 +23,13 @@ public class Main {
 		} else if (K == 26) {
 			bw.write(Integer.toString(N));
 		} else {
-			int learn = 1 << 26;
+			int learn = 0;
 			learn |= 1; // a
 			learn |= 1 << ('n' - 'a');
 			learn |= 1 << ('t' - 'a');
 			learn |= 1 << ('i' - 'a');
 			learn |= 1 << ('c' - 'a');
-			max = Integer.MIN_VALUE;
+			max = 0;
 			learnComb(0, 0, learn);
 			bw.write(Integer.toString(max));
 		}
@@ -42,8 +39,8 @@ public class Main {
 	static void learnComb(int startIdx, int select, int learn) {
 		if (select == K-5) {
 			int cnt = 0;
-			for (int i = 0; i < N; i++) {
-				if ((words[i] & learn) == words[i]) { cnt++; }
+			for (int word : words) {
+				if ((word & learn) == word) { cnt++; }
 			}
 			max = Math.max(max, cnt);
 			return;
@@ -51,9 +48,7 @@ public class Main {
 
 		for (int i = startIdx; i < 26; i++) {
 			if ((learn & (1 << i)) != 0) continue;
-			learn |= (1 << i);
-			learnComb(i + 1, select + 1, learn);
-			learn &= ~(1 << i);
+			learnComb(i + 1, select + 1, learn | (1 << i));
 		}
 	}
 
