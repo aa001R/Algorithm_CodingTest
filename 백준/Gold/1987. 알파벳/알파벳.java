0958e8731e4b452bb3956c1811076068d3e09edb
Kim Areum <101612927+aa001R@users.noreply.github.com>
2025-01-24 18:39:22 +0900

    [Gold IV] Title: 알파벳, Time: 544 ms, Memory: 12816 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -4,12 +4,9 @@ import java.io.*;
 public class Main {
 	static int max;
 	static int[][] board;
-	static boolean [] visited;
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		board = new int[read()+2][read()+2];
-		visited = new boolean[27];
 
 		for(int i = 1; i < board[0].length-1; i++) {
 			board[0][i] = board[board.length-1][i] = 91;
@@ -17,7 +14,6 @@ public class Main {
 		for(int i = 0; i < board.length; i++) {
 			board[i][0] = board[i][board[i].length-1] = 91;
 		}
-		visited[26] = true;
 
 		for(int i = 1; i < board.length-1; i++)
 			for(int j = 1; j < board[i].length-1; j++)
@@ -25,26 +21,24 @@ public class Main {
 
 
 		max = 1;
-		visited[board[1][1] - 65] = true;
-		dfs(1, 1, 1);
+		dfs(1, 1, 1, (1 << 26) | (1 << (board[1][1] - 65)));
 		bw.append(Integer.toString(max));
 		bw.flush();
 	}
 
 	static int [][]delta = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-	static void dfs(int r, int c, int cnt) {
-		if(visited[board[r+1][c] - 65] && visited[board[r][c+1] - 65]
-		&& visited[board[r-1][c] - 65] && visited[board[r][c-1] - 65]) {
+	static void dfs(int r, int c, int cnt, int visited) {
+		if( (visited & (1 << (board[r+1][c] - 65))) != 0 && (visited & (1 << (board[r-1][c] - 65))) != 0 && (visited & (1 << (board[r][c+1] - 65))) != 0 && (visited & (1 << (board[r][c-1] - 65))) != 0) {
 			max = Math.max(max, cnt);
 			return;
 		}
 		for(int d = 0; d < 4; d++) {
 			int nr = delta[d][0] + r;
 			int nc = delta[d][1] + c;
-			if (visited[board[nr][nc] - 65]) continue;
-			visited[board[nr][nc] - 65] = true;
-			dfs(nr, nc, cnt + 1);
-			visited[board[nr][nc] - 65] = false;
+			if ((visited & (1 << (board[nr][nc] - 65))) != 0) continue;
+			visited |= (1 << (board[nr][nc] - 65));
+			dfs(nr, nc, cnt + 1, visited);
+			visited &= ~(1 << (board[nr][nc] - 65));
 		}
 	}
 
