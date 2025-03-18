c52d48d39926b27d2e0ab614a55ce222d165b63d
Kim Areum <101612927+aa001R@users.noreply.github.com>
2025-03-19 06:08:04 +0900

    [level 2] Title: H-Index, Time: 4.41 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -2,33 +2,14 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] citations) {
-        int answer = 0;
         Arrays.sort(citations);
-        return answer = binarySearch(citations, 0, citations[citations.length - 1]);
-    }
-    
-    int binarySearch(int [] arr, int left, int right){
-        int h = 0;
-        while (left < right) {
-            int mid = (left + right) / 2;
-            if (check(mid, arr)) {
-                if (h < mid) h = mid;
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
+
+        int max = 0;
+        for(int i = citations.length-1; i > -1; i--){
+            int min = (int)Math.min(citations[i], citations.length - i);
+            if(max < min) max = min;
         }
-        return h;
+
+        return max;
     }
-    
-    boolean check(int mid, int [] arr){
-        int above = 0, below = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] <= mid) below++;
-            if (arr[i] >= mid) above++; 
-        }
-        if (mid <= above && mid >= below) return true;
-        return false;
-    }
-    
 }
