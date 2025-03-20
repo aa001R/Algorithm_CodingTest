47e5f1399a183165cecc2ad19678c794ae126fa5
Kim Areum <101612927+aa001R@users.noreply.github.com>
2025-03-21 03:12:08 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 73.05 ms, Memory: 97.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,11 +2,15 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        Arrays.sort(participant);
-        Arrays.sort(completion);
-        for (int i = 0; i<completion.length; i++) {
-            if (!participant[i].equals(completion[i])) return participant[i];
+        String answer = "";
+        HashMap<String, Integer> hm = new HashMap<>();
+        for (String player : participant) hm.put(player, hm.getOrDefault(player, 0) + 1);
+        for (String player : completion) hm.put(player, hm.get(player) - 1);
+        for (Map.Entry<String, Integer> player : hm.entrySet()){
+            if (player.getValue() == 0) continue; 
+            answer = player.getKey();
+            break;
         }
-        return participant[participant.length-1];
+        return answer;
     }
 }
