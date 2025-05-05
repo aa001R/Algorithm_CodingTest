aab00e7f3e08f62bca63884452cf1d0fe68a868c
Kim Areum <101612927+aa001R@users.noreply.github.com>
2025-05-06 01:55:39 +0900

    [level 3] Title: 이중우선순위큐, Time: 54.50 ms, Memory: 133 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -2,39 +2,30 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] operations) {
-        int[] answer = new int [2];
+        int[] answer = new int[2];
         TreeMap<Integer, Integer> tree = new TreeMap<>();
-        int opCnt = operations.length;
-        for (int i = 0; i < opCnt; i++) {
-				StringTokenizer st = new StringTokenizer(operations[i]);
-				char cmd = st.nextToken().charAt(0);
-				int num = Integer.parseInt(st.nextToken());
 
-				if (cmd == 'I')
-					tree.put(num, tree.getOrDefault(num, 0) + 1);
-				else if (cmd == 'D') {
-					if (tree.isEmpty()) continue;
+        for (String op : operations) {
+            StringTokenizer st = new StringTokenizer(op);
+            String cmd = st.nextToken();
+            int num = Integer.parseInt(st.nextToken());
 
-					int res = 0;
-					if (num == 1)
-						res = tree.lastKey();
-					else if (num == -1) {
-						res = tree.firstKey();
-					}
-					
-					if (tree.get(res) == 1)
-						tree.remove(res);
-					else
-						tree.put(res, tree.get(res) - 1);
-				}
-			}
-			if (tree.isEmpty()) {
-                answer[0] = answer[1] = 0;
+            if (cmd.equals("I")) {
+                tree.put(num, tree.getOrDefault(num, 0) + 1);
+            } else if (cmd.equals("D") && !tree.isEmpty()) {
+                int key = (num == 1) ? tree.lastKey() : tree.firstKey();
+                if (tree.get(key) == 1) {
+                    tree.remove(key);
+                } else {
+                    tree.put(key, tree.get(key) - 1);
+                }
             }
-			else {
-                answer[0] = tree.lastKey();
-                answer[1] = tree.firstKey();
-			}
+        }
+
+        if (!tree.isEmpty()) {
+            answer[0] = tree.lastKey();
+            answer[1] = tree.firstKey();
+        }
         return answer;
     }
 }
