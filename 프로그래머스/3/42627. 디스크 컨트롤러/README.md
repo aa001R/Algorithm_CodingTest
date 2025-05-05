4c4654c989219208d5170df38954261672ddd3a0
Kim Areum <101612927+aa001R@users.noreply.github.com>
2025-05-06 01:32:06 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 2.77 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/README.md
+++ 프로그래머스/3/42627. 디스크 컨트롤러/README.md
@@ -1,10 +1,10 @@
 # [level 3] 디스크 컨트롤러 - 42627 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627) 
 
 ### 성능 요약
 
-메모리: 71.8 MB, 시간: 2.46 ms
+메모리: 75.3 MB, 시간: 2.77 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 19일 04:19:37
+2025년 05월 06일 01:32:04
 
 ### 문제 설명
 
--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -10,25 +10,26 @@ class Solution {
         // 작업의 소요시간 기준으로 오름차순 정렬
         PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
 
-        int jobs_index = 0; // 작업 배열 인덱스
-        int finish_job = 0; // 처리 완료된 작업 개수
-        int end_time = 0; // 작업 처리 완료 시간
+        int now = 0; // 현재 시간
+        int jobIndex = 0; // 작업 배열 인덱스
+        int completed = 0; // 처리 완료된 작업 개수
 
-        while(true) {
-            if(finish_job == jobs.length) break; // 모든 작업을 처리했다면 종료
-
-            // 이전 작업 처리 중 요청된 작업 add
-            while(jobs_index < jobs.length && jobs[jobs_index][0] <= end_time) {
-                pq.add(jobs[jobs_index++]);
+		// 모든 작업을 처리했다면 종료
+        while (completed < jobs.length) {
+        
+            // 현재 시간까지 들어온 작업(이전 작업 처리 중 요청된 작업)을 모두 우선순위 큐에 추가
+            while (jobIndex < jobs.length && jobs[jobIndex][0] <= now) {
+                pq.add(jobs[jobIndex++]);
             }
 
             if(!pq.isEmpty()) { // 이전 작업 처리 중 요청된 작업이 있는 경우
                 int[] job = pq.poll();
-                answer += end_time - job[0] + job[1]; // 작업 요청부터 종료까지 걸린 시간 추가
-                end_time += job[1]; // 작업 처리 완료 시간 갱신
-                finish_job++; // 처리 완료된 작업 개수 1 증가
+                now += job[1]; // 작업 수행 후 시간 증가
+                answer += now - job[0]; // 요청 시점부터 종료까지 걸린 시간 누적
+                completed++; // 처리 완료된 작업 개수 1 증가
             } else { // 이전 작업 처리 중 요청된 작업이 없는 경우
-                end_time = jobs[jobs_index][0]; // 다음 작업 요청 시점으로 갱신
+                // 처리할 작업이 없다면 다음 작업 요청 시간으로 점프
+                now = jobs[jobIndex][0];
             }
         }
 
