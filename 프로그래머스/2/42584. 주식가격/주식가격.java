2eea5f331e1e1ec549a0baf0ea9906dbcc8ae4a1
Kim Areum <101612927+aa001R@users.noreply.github.com>
2025-05-13 23:39:26 +0900

    [level 2] Title: 주식가격, Time: 39.87 ms, Memory: 92 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,15 +1,26 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] prices) {
-        int len = prices.length;
-        int[] answer = new int[len];
-        int i, j;
-        for (i = 0; i < len; i++) {
-            for (j = i + 1; j < len; j++) {
-                answer[i]++;
-                if (prices[i] > prices[j])
-                    break;
+        int n = prices.length;
+        int[] answer = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            // 현재 가격이 스택의 top보다 작으면 이전 가격이 떨어진 것
+            while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
+                int idx = stack.pop();
+                answer[idx] = i - idx;
             }
+            stack.push(i); // 인덱스를 저장
         }
+
+        // 끝까지 떨어지지 않은 가격 처리
+        while (!stack.isEmpty()) {
+            int idx = stack.pop();
+            answer[idx] = n - idx - 1;
+        }
+
         return answer;
     }
 }
