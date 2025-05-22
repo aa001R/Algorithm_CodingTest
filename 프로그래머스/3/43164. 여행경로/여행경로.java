f1eed5a10291d4cb46986ca16dcc51f119ebb63e
Kim Areum <101612927+aa001R@users.noreply.github.com>
2025-05-23 05:04:07 +0900

    [level 3] Title: 여행경로, Time: 0.82 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,52 +1,27 @@
 import java.util.*;
 
 class Solution {
-    class Airport implements Comparable<Airport>{
-        String name;
-        boolean isArrived;
-        Airport(String name){
-            this.name = name;
-            this.isArrived = false;
-        }
-        @Override
-        public int compareTo(Airport other){
-            return this.name.compareTo(other.name);   
-        }
-    }
-    
-    static HashMap<String, ArrayList<Airport>> airports;
-    public String[] solution(String[][] tickets) {
-        airports = new HashMap<>();
-        for (String [] ticket : tickets){
-            airports.computeIfAbsent(ticket[0], k -> new ArrayList<>())
-                    .add(new Airport(ticket[1]));
-        }
-        
-        for (String key : airports.keySet()) {
-            Collections.sort(airports.get(key));
-        }
-        
-        String [] ans = new String[tickets.length+1];
-        ans[0] = "ICN";
-        dfs(1, ans);
-        return ans;
-    }
-    
-    boolean dfs (int depth, String [] visited){
-        if (depth >= visited.length){
-           return true; 
-        }
-        List<Airport> nextList = airports.get(visited[depth - 1]);
-        if (nextList == null) return false;
+    Map<String, PriorityQueue<String>> graph = new HashMap<>();
+    LinkedList<String> path = new LinkedList<>();
 
-        for (Airport des : nextList) {
-            if (des.isArrived) continue;
-            des.isArrived = true;
-            visited[depth] = des.name;
-            if (dfs(depth + 1, visited)) return true;
-            des.isArrived = false;
+    public String[] solution(String[][] tickets) {
+        for (String[] ticket : tickets) {
+            graph.computeIfAbsent(ticket[0], k -> new PriorityQueue<>())
+                 .add(ticket[1]);
         }
-        return false;
+
+        dfs("ICN");
+
+        return path.toArray(new String[0]);
     }
-    
+
+    void dfs(String airport) {
+        PriorityQueue<String> arrivals = graph.get(airport);
+
+        while (arrivals != null && !arrivals.isEmpty()) {
+            dfs(arrivals.poll()); // 사전순 우선 방문
+        }
+
+        path.addFirst(airport); // 역순으로 추가
+    }
 }
