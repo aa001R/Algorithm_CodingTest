819ec561f488a5ad3d8eb0dea53338cbdc2fb13d
Kim Areum <101612927+aa001R@users.noreply.github.com>
2025-05-22 22:50:43 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 11.37 ms, Memory: 54.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,36 +2,65 @@ import java.util.*;
 
 class Solution {
     static int n, m;
+
     public int solution(int[][] maps) {
-        int answer = 0;
         n = maps.length;
         m = maps[0].length;
-        int [][] walks = new int[n][m];
-        int [][] delta = {{-1, 0},{1, 0},{0, 1},{0, -1}};
-        ArrayDeque<int []> que = new ArrayDeque<>();
-        walks[0][0] = 1;
-        que.offer(new int[]{0, 0});
-        
-        while(!que.isEmpty()){
-            int [] cur = que.poll();
-            for (int d = 0; d < 4; d++) {
-                int nr = cur[0] + delta[d][0];
-                int nc = cur[1] + delta[d][1];
-                if (isOut(nr, nc) || maps[nr][nc] == 0 || walks[nr][nc] != 0) continue;
-                walks[nr][nc] = walks[cur[0]][cur[1]] + 1;
-                que.offer(new int[] {nr, nc});
+        int[][] gCost = new int[n][m];
+        boolean[][] visited = new boolean[n][m];
+        int[][] dirs = {{-1,0},{1,0},{0,-1},{0,1}};
+
+        for (int[] row : gCost) Arrays.fill(row, Integer.MAX_VALUE);
+        gCost[0][0] = 1;
+
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.offer(new Node(0, 0, 1, h(0, 0)));
+
+        while (!pq.isEmpty()) {
+            Node cur = pq.poll();
+            int r = cur.r, c = cur.c;
+
+            if (visited[r][c]) continue;
+            visited[r][c] = true;
+
+            if (r == n-1 && c == m-1) return cur.g;
+
+            for (int[] d : dirs) {
+                int nr = r + d[0], nc = c + d[1];
+                if (nr < 0 || nr >= n || nc < 0 || nc >= m) continue;
+                if (maps[nr][nc] == 0 || visited[nr][nc]) continue;
+
+                int newG = cur.g + 1;
+                if (newG < gCost[nr][nc]) {
+                    gCost[nr][nc] = newG;
+                    pq.offer(new Node(nr, nc, newG, h(nr, nc)));
+                }
             }
         }
-                
-        if(walks[n-1][m-1] != 0) {
-            answer = walks[n-1][m-1];
-        } else {
-            answer = -1;
-        }
-        return answer;
+
+        return -1;
     }
-    
-    public boolean isOut(int r, int c){
-        return r < 0 || r >= n || c < 0 || c >= m;
+
+    // 맨해튼 거리
+    private int h(int r, int c) {
+        return Math.abs(n - 1 - r) + Math.abs(m - 1 - c);
+    }
+
+    class Node implements Comparable<Node> {
+        int r, c;
+        int g; // 시작점부터 현재까지의 거리
+        int f; // g + h
+
+        Node(int r, int c, int g, int h) {
+            this.r = r;
+            this.c = c;
+            this.g = g;
+            this.f = g + h;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return this.f - o.f;
+        }
     }
 }
