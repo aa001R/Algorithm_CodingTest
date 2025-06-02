27ab7bfa09f2adeb28978fa2fa8d9a57ea6bfe3f
Kim Areum <101612927+aa001R@users.noreply.github.com>
2025-06-02 16:46:21 +0900

    [level 2] Title: 모음 사전, Time: 5.26 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -0,0 +1,18 @@
+class Solution {
+    static int order;
+    static String [] ALPHA = {"A","E","I","O","U"};
+    public int solution(String word) {
+        findOrderDfs("", word);
+        return order;
+    }
+    
+    private boolean findOrderDfs(String str, String word){
+        if (str.equals(word)) return true;        
+        if (str.length() == 5) return false;
+        for (int i = 0; i < 5; i++) {
+            order++;
+            if (findOrderDfs(str+ALPHA[i], word)) return true;
+        }
+        return false;
+    }
+}
