0ac886924ff462803f4aa6b5c8eeceec583538ca
Kim Areum <101612927+aa001R@users.noreply.github.com>
2025-06-04 22:11:54 +0900

    [level 3] Title: 정수 삼각형, Time: 9.81 ms, Memory: 65.7 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/README.md
+++ 프로그래머스/3/43105. 정수 삼각형/README.md
@@ -1,10 +1,10 @@
 # [level 3] 정수 삼각형 - 43105 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43105?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43105) 
 
 ### 성능 요약
 
-메모리: 63.4 MB, 시간: 11.03 ms
+메모리: 65.7 MB, 시간: 9.81 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 04일 21:52:00
+2025년 06월 04일 22:11:53
 
 ### 문제 설명
 
--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,14 +1,27 @@
-import java.util.*;
-
 class Solution {
+    int[][] memo;
+    int[][] triangle;
+
     public int solution(int[][] triangle) {
         int h = triangle.length;
-        for (int i = 1; i < h; i++) {
-            triangle[i][0] += triangle[i-1][0];
-            triangle[i][i] += triangle[i-1][i-1];
-            for (int j = 1; j < i; j++) 
-                triangle[i][j] += Math.max(triangle[i-1][j-1], triangle[i-1][j]);
-        }
-        return Arrays.stream(triangle[h-1]).max().getAsInt();
+        this.triangle = triangle;
+        this.memo = new int[h][h];
+
+        return dfs(0, 0); // 꼭대기부터 시작
+    }
+
+    private int dfs(int i, int j) {
+        // 마지막 줄이면 자기 자신 반환
+        if (i == triangle.length - 1) return triangle[i][j];
+
+        // 이미 계산한 값이면 바로 반환
+        if (memo[i][j] != 0) return memo[i][j];
+
+        // 아래 두 경로 중 큰 값 선택
+        int left = dfs(i + 1, j);
+        int right = dfs(i + 1, j + 1);
+        memo[i][j] = triangle[i][j] + Math.max(left, right);
+
+        return memo[i][j];
     }
 }
