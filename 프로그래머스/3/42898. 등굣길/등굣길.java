0d91346974bc7454b16901b84b5eb72bc9c00dac
Kim Areum <101612927+aa001R@users.noreply.github.com>
2025-06-09 19:34:03 +0900

    [level 3] Title: 등굣길, Time: 0.65 ms, Memory: 54.6 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -9,7 +9,10 @@ class Solution {
         board[1][1] = 1;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
-                if (board[i][j] < 0) {board[i][j] = 0; continue;}
+                if (board[i][j] < 0) {
+                    board[i][j] = 0; 
+                    continue;
+                }
                 board[i][j] += (board[i-1][j] + board[i][j-1]) % 1_000_000_007;
             }
         }
