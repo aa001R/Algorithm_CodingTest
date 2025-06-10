4284b561caa41b916fe7f9f87bae76d0f1196172
Kim Areum <101612927+aa001R@users.noreply.github.com>
2025-06-10 20:19:53 +0900

    [level 4] Title: 도둑질, Time: 23.77 ms, Memory: 95.8 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.java
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -1,12 +1,17 @@
 class Solution {
     public int solution(int[] money) {
-        if (money.length == 3)
+        int n = money.length;
+        if (n == 3) {
             return Math.max(money[0], Math.max(money[1], money[2]));
-        return Math.max(robHouse(0, money.length - 2, money), robHouse(1, money.length - 1, money));
+        }
+        
+        // Case 1: 첫 집 고려, 마지막 집 제외 (0 ~ n-2)
+        // Case 2: 첫 집 제외, 마지막 집 고려 (1 ~ n-1)  
+        return Math.max(robHouse(money, 0, n-2), robHouse(money, 1, n-1));
     }
     
     // 일직선 배치에서의 최대 금액을 구하는 함수
-    private int robHouse(int start, int end, int [] money){
+    private int robHouse(int[] money, int start, int end){
         int [] dp = new int[money.length];
         dp[start] = money[start];
         dp[start + 1] = Math.max(money[start], money[start + 1]);
