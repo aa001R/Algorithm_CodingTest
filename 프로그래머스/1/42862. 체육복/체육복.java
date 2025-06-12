856ac9e970bc5391bf94e9c804fd35008a207a73
Kim Areum <101612927+aa001R@users.noreply.github.com>
2025-06-13 01:33:53 +0900

    [level 1] Title: 체육복, Time: 0.03 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -2,25 +2,23 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int answer = n - lost.length;
-        boolean [] isReserve = new boolean[n+2];
-        Arrays.sort(lost);
-        for (int idx : reserve){
-            isReserve[idx] = true;
-        }
-        for(int i = 0; i < lost.length; i++){
-            if (isReserve[lost[i]]) {
-                isReserve[lost[i]] = false;
-                lost[i] = 0;
-                answer++;
-            }
-        }
-        for(int lostStu : lost){
-            if (lostStu == 0) continue;
-            else if (isReserve[lostStu - 1]) isReserve[lostStu - 1] = false;
-            else if (isReserve[lostStu+1]) isReserve[lostStu + 1] = false;
-            else continue;
-            answer++;
+        int[] people = new int[n+2];
+        int answer = n;
+
+        for (int l : lost) 
+            people[l]--;
+        for (int r : reserve) 
+            people[r]++;
+
+        for (int i = 1; i <= n; i++) {
+            if(people[i] != -1) continue;
+            if(people[i-1] == 1) {
+                people[i]++;
+                people[i-1]--;
+            }else if(people[i+1] == 1) {
+                people[i]++;
+                people[i+1]--;
+            }else answer--;
         }
         return answer;
     }
