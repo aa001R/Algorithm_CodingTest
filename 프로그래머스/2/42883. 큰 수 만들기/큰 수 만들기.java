af6519c1f9659c2daac7c08a3abb706305d3c84e
Kim Areum <101612927+aa001R@users.noreply.github.com>
2025-06-13 02:07:30 +0900

    [level 2] Title: 큰 수 만들기, Time: 85.25 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,28 +1,22 @@
+import java.util.*;
 class Solution {
     public String solution(String number, int k) {
         StringBuilder answer = new StringBuilder();
-        boolean [] remove = new boolean[number.length()];
-        for (int i = 0; i < number.length() && k > 0; i++) {
-            if (remove[i]) continue;
-            for (int j = i + 1; j < number.length() && j <= i + k; j++) {
-                if (remove[j]) continue;
-                if (number.charAt(i) < number.charAt(j)) {
-                    remove[i] = true;
+        ArrayDeque<Character> stack = new ArrayDeque<>();
+        for (int i = 0; i < number.length(); i++) {
+            while( !stack.isEmpty() && stack.peek() < number.charAt(i) && k > 0) {
+                    stack.pop();
                     k--;
-                    break;
-                }
             }
+            stack.push(number.charAt(i));
         }
-        for (int i = number.length() - 1; i >= 0 && k > 0; i--) {
-            if (remove[i]) continue;
-            remove[i] = true;
+        while( !stack.isEmpty() && k > 0) {
+            stack.pop();
             k--;
         }
-        
-        for (int i = 0; i < number.length(); i++) {
-            if (remove[i]) continue;
-            answer.append(number.charAt(i));
+        while( !stack.isEmpty()) {
+            answer.append(stack.pop());
         }
-        return answer.toString();
+        return answer.reverse().toString();
     }
 }
