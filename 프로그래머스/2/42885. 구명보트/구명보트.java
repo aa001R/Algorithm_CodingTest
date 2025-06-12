155ced421640f83f0a5146e65c3a77a6b4c6dae1
Kim Areum <101612927+aa001R@users.noreply.github.com>
2025-06-13 02:32:35 +0900

    [level 2] Title: 구명보트, Time: 11.80 ms, Memory: 57.8 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -6,17 +6,11 @@ class Solution {
         Arrays.sort(people);
         int left = 0, right = people.length - 1;
         while (left < right) {
-            int total = people[left] + people[right];
+            int total = people[left] + people[right--];
             if (total <= limit) {
-                people[left++] = people[right--] = 0;
-                answer++;
-            } else {
-                right--;
+                left++;
             }
         }
-        for (int i = left; i < people.length; i++) {
-            if (people[i] != 0) answer++;
-        }
-        return answer;
+        return people.length - left;
     }
 }
