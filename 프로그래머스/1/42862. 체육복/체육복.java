5c1cd3321fb923562bad993cfc7556553e3f78e5
Kim Areum <101612927+aa001R@users.noreply.github.com>
2025-06-14 23:01:24 +0900

    [level 1] Title: 체육복, Time: 0.21 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -2,25 +2,30 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int[] students = new int[n+2];
-        int answer = n;
+        Set<Integer> lostSet = new HashSet<>();
+        Set<Integer> reserveSet = new HashSet<>();
 
-        for (int l : lost) 
-            students[l]--;
-        for (int r : reserve) 
-            students[r]++;
+        // 일단 다 추가
+        for (int l : lost) lostSet.add(l);
+        for (int r : reserve) reserveSet.add(r);
 
-        for (int i = 1; i <= n; i++) {
-            if(students[i] != -1) continue;
-            if(students[i-1] == 1) {
-                students[i]++;
-                students[i-1]--;
-            }else if(students[i+1] == 1) {
-                students[i]++;
-                students[i+1]--;
-            }else answer--;
+        // 여벌이 있는데 도난당한 경우 제거
+        Set<Integer> overlap = new HashSet<>(lostSet);
+        overlap.retainAll(reserveSet); // 교집합
+        lostSet.removeAll(overlap);
+        reserveSet.removeAll(overlap);
+
+        // 앞뒤 학생 확인해서 빌려줄 수 있으면 처리
+        for (int l : new HashSet<>(lostSet)) {
+            if (reserveSet.contains(l - 1)) {
+                reserveSet.remove(l - 1);
+                lostSet.remove(l);
+            } else if (reserveSet.contains(l + 1)) {
+                reserveSet.remove(l + 1);
+                lostSet.remove(l);
+            }
         }
-        return answer;
-    }
 
+        return n - lostSet.size();  // 체육복 못 받은 학생 수만큼 차감
+    }
 }
