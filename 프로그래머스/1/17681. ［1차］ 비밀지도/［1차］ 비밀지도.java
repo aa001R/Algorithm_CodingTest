89add869bb9600fe09e1132f9113a38bcf37d497
Kim Areum <101612927+aa001R@users.noreply.github.com>
2025-10-10 21:06:49 +0900

    [level 1] Title: [1차] 비밀지도, Time: 9.55 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,17 +1,20 @@
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = new String[n];
-        for (int i = 0; i < n; i++){
-            // 두 지도 행 합치기
+        String[] result = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            // 비트 OR 연산으로 두 지도를 합침
             int merged = arr1[i] | arr2[i];
-            // 0, 1 검사 및 # 변환
-            StringBuilder sb = new StringBuilder();
-            for (int j = n - 1; j >= 0; j--) {
-                if ((merged & (1 << j)) == 0) sb.append(" ");
-                else sb.append("#");
-            }
-            answer[i] = sb.toString();
+
+            // 이진수 문자열로 변환 후 왼쪽을 0으로 채워 길이 맞춤
+            String binary = String.format("%" + n + "s", Integer.toBinaryString(merged));
+
+            // '1'은 '#', '0'은 ' ' 로 변환
+            binary = binary.replace('1', '#').replace('0', ' ');
+
+            result[i] = binary;
         }
-        return answer;
+
+        return result;
     }
 }
