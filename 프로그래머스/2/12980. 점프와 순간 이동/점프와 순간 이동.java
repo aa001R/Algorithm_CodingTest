00912361fc246279853bd97c75d1198f07b00e81
Kim Areum <101612927+aa001R@users.noreply.github.com>
2025-10-17 04:04:54 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.02 ms, Memory: 71.1 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -2,11 +2,6 @@ import java.util.*;
 
 public class Solution {
     public int solution(int n) {
-        int ans = 0;
-        while(n > 0){
-            ans += n % 2 == 1 ? 1 : 0;
-            n /= 2;
-        }
-        return ans;
+        return Integer.bitCount(n); // n의 1의 개수 
     }
 }
