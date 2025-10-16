1254b4ada75b5c47a7b4762cd1c2ded1352faf24
Kim Areum <101612927+aa001R@users.noreply.github.com>
2025-10-17 00:24:29 +0900

    [level 2] Title: 숫자의 표현, Time: 0.10 ms, Memory: 53.6 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,16 +1,14 @@
 class Solution {
     public int solution(int n) {
-        int answer = 1; // n = n
+        // int answer = 1; // n = n
         /* n = a + (a+1) + (a+2) + ... + (a+i-1) : i개 연속 수
         n = i * (a+(a+i-1))/2 = i * (2a+i-1)/2
         a = n/i - (i-1)/2 >= 1        
         a = (n - i(i-1)/2) / i : a는 정수
         */
-        for (int len = 2; len * (len + 1) / 2 <= n; len++) {
-            if ((n - len * (len - 1) / 2) % len == 0) {
-                answer++;
-            }
-        }
+        int answer = 0;
+        for (int i = 1; i <= n; i += 2) 
+            if (n % i == 0) answer++;
         return answer;
     }
 }
