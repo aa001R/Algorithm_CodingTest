4cd3faf25e0658e8cb1dd4a300e7aa955a4dae90
Kim Areum <101612927+aa001R@users.noreply.github.com>
2025-10-23 01:22:39 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.64 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -1,42 +1,23 @@
 import java.util.*;
 
 class Solution {
-    /* 
-    mbti, 7개 대답별 점수
-    RT, CF, JM, AN
-    매우 3, 기본 2, 약간 1, 모르겠음 0
-    결과 = 성격 유형 합산 더 높은 점수 > 사전순
-    survey[i] = 비동의동의
-    성격 유형 return
-    */
     public String solution(String[] survey, int[] choices) {
-        int n = survey.length;
-        int[] scores = new int[8];
-        String[] personality = {"RT", "CF", "JM", "AN"};
-        Map<Character, Integer> total = new HashMap<>();
-        for(int i = 1; i <= 3; i++) {
-            scores[i] = scores[8 - i] = 4 - i;
+        int[] score = {0, 3, 2, 1, 0, 1, 2, 3};
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : "RTCFJMAN".toCharArray()) map.put(c, 0);
+        
+        for (int i = 0; i < survey.length; i++) {
+            char a = survey[i].charAt(0), b = survey[i].charAt(1);
+            int choice = choices[i];
+            if (choice < 4) map.put(a, map.get(a) + score[choice]);
+            else if (choice > 4) map.put(b, map.get(b) + score[choice]);
         }
-        for(String p : personality){
-            total.put(p.charAt(0), 0);
-            total.put(p.charAt(1), 0);
-        }
-        for(int i = 0; i < n; i++){
-            char disagree = survey[i].charAt(0);
-            char agree = survey[i].charAt(1);
-            if(choices[i] < 4){ // 비동의
-                total.put(disagree, total.get(disagree) + scores[choices[i]]);
-            } else if(choices[i] > 4){ // 동의
-                total.put(agree, total.get(agree) + scores[choices[i]]);
-            }
-        }
+
         StringBuilder sb = new StringBuilder();
-        for(String p : personality){
-            char a = p.charAt(0);
-            char b = p.charAt(1);
-            if(total.get(a) >= total.get(b)) sb.append(a);
-            else sb.append(b);
-        }
+        sb.append(map.get('R') >= map.get('T') ? 'R' : 'T');
+        sb.append(map.get('C') >= map.get('F') ? 'C' : 'F');
+        sb.append(map.get('J') >= map.get('M') ? 'J' : 'M');
+        sb.append(map.get('A') >= map.get('N') ? 'A' : 'N');
         return sb.toString();
     }
 }
