add9c788d93e7568a44bf4ca16a4b443f9155b12
Kim Areum <101612927+aa001R@users.noreply.github.com>
2025-10-23 02:25:22 +0900

    [level 1] Title: 신규 아이디 추천, Time: 14.62 ms, Memory: 91 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -11,9 +11,8 @@ class Solution {
     // 5단계 new_id가 빈 문자열이라면, new_id에 "a"를 대입합니다.
         if(answer.isEmpty()) answer += "a";
     // 6단계 new_id의 길이가 16자 이상이면, new_id의 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거합니다.
-        if(answer.length() >= 16) answer = answer.substring(0, 15);
     // 만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거합니다.
-        if(answer.endsWith(".")) answer = answer.substring(0, answer.length()-1);
+        if(answer.length() >= 16) answer = answer.substring(0, 15).replaceAll("\\.$", "");
     // 7단계 new_id의 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙입니다.
         while(answer.length() <= 2) answer += answer.charAt(answer.length()-1);
         return answer;
