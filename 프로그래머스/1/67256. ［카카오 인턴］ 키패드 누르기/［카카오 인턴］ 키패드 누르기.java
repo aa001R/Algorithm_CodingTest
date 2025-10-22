9b21b2ac8836918f412462f7a8ed1dd9ce22b091
Kim Areum <101612927+aa001R@users.noreply.github.com>
2025-10-23 01:53:33 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.35 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -1,38 +1,16 @@
 class Solution {
-    /* 상하좌우 1칸
-    147 왼손, 369 오른손, 2580 더 가까운 엄지 = 같으면 hand
-    */
     public String solution(int[] numbers, String hand) {
-        // 각 key = 4*3, 0-base로 매칭
-        /*  012
-            345
-            678
-            91011
-        */
         int left = 9, right = 11;
         StringBuilder sb = new StringBuilder();
-        for(int num : numbers){
-            switch(num){
-                case 1:case 4:case 7:
-                    left = num - 1;
-                    sb.append("L");
-                    break;
-                case 3:case 6:case 9:
-                    right = num - 1;
-                    sb.append("R");
-                    break;
-                default:
-                    num = (num-1) > 0 ? num-1 : 10;
-                    int lDis = Math.abs((left/3) - (num/3)) + Math.abs((left%3) - (num%3));
-                    int rDis = Math.abs((right/3) - (num/3)) + Math.abs((right%3) - (num%3));
-                    if(lDis < rDis || (lDis == rDis && hand.equals("left"))){
-                        left = num;
-                        sb.append("L");
-                    }else {
-                        right = num;
-                        sb.append("R");                        
-                    }
-                    break;
+        for(int n : numbers){
+            int num = (n == 0) ? 10 : n - 1;
+            if (n == 1 || n == 4 || n == 7) { left = num; sb.append('L'); }
+            else if (n == 3 || n == 6 || n == 9) { right = num; sb.append('R'); }
+            else {
+                int l = Math.abs(left / 3 - num / 3) + Math.abs(left % 3 - num % 3);
+                int r = Math.abs(right / 3 - num / 3) + Math.abs(right % 3 - num % 3);
+                if (l < r || (l == r && hand.equals("left"))) { left = num; sb.append('L'); }
+                else { right = num; sb.append('R'); }
             }
         }
         return sb.toString();
