2cebcc21893cc529c4bf138214f6186d15ef6e87
Kim Areum <101612927+aa001R@users.noreply.github.com>
2025-10-23 03:25:13 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 8.27 ms, Memory: 84.2 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -8,19 +8,23 @@ class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
         List<Integer> answer = new ArrayList<>();
         Map<String, Integer> termsMap = new HashMap<>();
-        // 월을 일 기준으로 변경
-        int[] date = Arrays.stream(today.split("\\.")).mapToInt(Integer::parseInt).toArray();
-        int todayD =  date[0] * 12 * 28 + date[1] * 28 + date[2];
+        int todayD = toDays(today); // 오늘 날짜를 일 단위로 변환
         for(String term : terms){
             String[] info = term.split(" ");
-            termsMap.put(info[0], Integer.parseInt(info[1])*28);
+            termsMap.put(info[0], Integer.parseInt(info[1]) * 28);
         }
         for(int i = 0; i < privacies.length; i++){
             String[] p = privacies[i].split(" ");
-            date = Arrays.stream(p[0].split("\\.")).mapToInt(Integer::parseInt).toArray();
-            int expirationD = date[0] * 12 * 28 + date[1] * 28 + date[2] + termsMap.get(p[1]) - 1;
-            if(todayD > expirationD) answer.add(i+1);
+            String dateStr = p[0];
+            String termType = p[1];
+            int expirationD = toDays(dateStr) + termsMap.get(termType) - 1;
+            if(todayD > expirationD) answer.add(i + 1);
         }
         return answer.stream().mapToInt(Integer::valueOf).toArray();
+    }
+    
+    private int toDays(String dateStr) {
+        int[] d = Arrays.stream(dateStr.split("\\.")).mapToInt(Integer::parseInt).toArray();
+        return d[0] * 12 * 28 + d[1] * 28 + d[2];
     }
 }
