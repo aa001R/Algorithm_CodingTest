a00d909b949f36efc8d7fef9cadd595395bf67bd
Kim Areum <101612927+aa001R@users.noreply.github.com>
2025-10-24 00:31:07 +0900

    [level 2] Title: 주차 요금 계산, Time: 21.87 ms, Memory: 93.5 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -4,41 +4,35 @@ class Solution {
     /*
     입차된 후 출차 내역 없다면 23:59 출차된 것으로 간주
     초과 시간이 단위시간으로 나누어 떨어지지 않으면 올림
-    시각 차량번호 내역
+    "시각 차량번호 내역"
     마지마 시각 입차된 경우 없음
+    비용 = 기본 비용 + ceiling(max(출차시간 - 입차시간 - 기본시간, 0) / 단위시간) * 단위 요금 
     */
     public int[] solution(int[] fees, String[] records) {
         List<Integer> answer = new ArrayList<>();
-        Map<String, int[]> recordMap = new TreeMap<>(); // 차번호 : {입차시간, 출차시간, 누적 시간}
-        int lastTime = getTime("23:59");
-        for(String _record : records){
-            String [] record = _record.split(" ");
-            int time = getTime(record[0]);
-            if(record[2].equals("IN")){
-                int period = 0;
-                if(recordMap.containsKey(record[1])) period = recordMap.get(record[1])[2];
-                recordMap.put(record[1], new int[]{time, -1, period});
-            }else{
-                int [] carInfo = recordMap.get(record[1]);
-                carInfo[1] = time;
-                carInfo[2] += carInfo[1] - carInfo[0];
-            }
+        Map<String, Integer> recordMap = new TreeMap<>(); // 차번호 : 누적 시간
+        int limitT = getTime("23:59");
+        
+        for(String record : records) {
+            String temp[] = record.split(" ");
+            int time = temp[2].equals("IN") ? -1 : 1;
+            time *= getTime(temp[0]);
+            recordMap.put(temp[1], recordMap.getOrDefault(temp[1], 0) + time); // -입차시간 +출차시간
         }
-        for(String carN : recordMap.keySet()){
-            int[] carInfo = recordMap.get(carN);
-            if(carInfo[1] < 0){
-                carInfo[2] += lastTime - carInfo[0];
-            }
-            int price = fees[1] + (int)Math.ceil(Math.max(carInfo[2] - fees[0], 0) * 1.0 / fees[2]) * fees[3];
-            answer.add(price);
+        
+        int idx = 0, ans[] = new int[recordMap.size()];
+        for(int time : recordMap.values()) {
+            if(time < 1) time += limitT;
+            time -= fees[0];
+            int cost = fees[1];
+            if(time > 0) cost += (time%fees[2] == 0 ? time/fees[2] : time/fees[2]+1) * fees[3];
+            ans[idx++] = cost;
         }
-        return answer.stream().mapToInt(Integer::valueOf).toArray();
+        return ans;
     }
     
-    private int getTime(String time){
-        return Arrays.stream(time.split(":"))
-                    .mapToInt(Integer::parseInt)
-                    .reduce((h, m)-> h*60 + m)
-                    .getAsInt();
+    private int getTime(String recordT){
+        String[] time = recordT.split(":");
+        return Integer.parseInt(time[0]) * 60 + Integer.parseInt(time[1]);
     }
 }
