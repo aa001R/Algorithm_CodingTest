cfcf0b339282fdd7c3249044988508b30e88ed28
Kim Areum <101612927+aa001R@users.noreply.github.com>
2025-10-24 01:07:15 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 31.05 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -2,45 +2,25 @@ import java.util.*;
 import java.util.regex.*;
 
 class Solution {
-    /*
-    HEAD NUMBER TAIL
-    숫자전문자 숫자{1, 5} 나머지 
-    
-    1. HEAD 기준 사전 순 대소문자 구문 x
-    2. NUMBER 숫자순 숫자 앞의 0은 무시
-    3. 입력 순서
-    */
-    class FileName implements Comparable<FileName>{
-        String head, number, tail;
-        int num;
-        FileName(String head, String number, String tail){
-            this.head = head;
-            this.number = number;
-            num = Integer.parseInt(number);
-            this.tail = tail;
-        }
-        @Override
-        public int compareTo(FileName other){
-            int headCompare = this.head.compareToIgnoreCase(other.head);
-            if (headCompare == 0) {
-                return Integer.compare(this.num, other.num);
-            }
-            return headCompare;
-        }
-        @Override
-        public String toString(){
-            return head+number+tail;            
-        }
-    }
     public String[] solution(String[] files) {
-        List<FileName> answer = new ArrayList<>();
-        Pattern p = Pattern.compile("([A-Za-z .-]+)(\\d{1,5})(.*)");
-        for(String file : files){
-            Matcher m = p.matcher(file);
-            m.find();
-            answer.add(new FileName(m.group(1), m.group(2), m.group(3)));
-        }
-        Collections.sort(answer);
-        return answer.stream().map(a -> a.toString()).toArray(String[]::new);
+        Pattern p = Pattern.compile("([a-z\\s.-]+)([0-9]{1,5})");
+
+        Arrays.sort(files, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                Matcher m1 = p.matcher(s1.toLowerCase());
+                Matcher m2 = p.matcher(s2.toLowerCase());
+                m1.find();
+                m2.find();
+
+                if(!m1.group(1).equals(m2.group(1))) {
+                    return m1.group(1).compareTo(m2.group(1));
+                } else {
+                    return Integer.parseInt(m1.group(2)) - Integer.parseInt(m2.group(2));
+                }
+            }
+        });
+
+        return files;
     }
 }
