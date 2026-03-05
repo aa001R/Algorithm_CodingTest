695fd324dda468378a5ced2a7b6895a9b77bcf8e
Kim Areum <101612927+aa001R@users.noreply.github.com>
2026-03-06 00:45:25 +0900

    [Silver II] Title: 오락실에 간 총총이, Time: 212 ms, Memory: 21084 KB -BaekjoonHub

--- 백준/Silver/26071. 오락실에 간 총총이/오락실에 간 총총이.java
+++ 백준/Silver/26071. 오락실에 간 총총이/오락실에 간 총총이.java
@@ -7,42 +7,19 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		min = Integer.MAX_VALUE;
-		char[][] board = new char[N][N];
 		int maxR = -1, maxC = -1, minR = N, minC = N;
 		for (int r = 0; r < N; r++) {
 			String input = br.readLine();
 			for (int c = 0; c < N; c++){
-				board[r][c] = input.charAt(c);
-				if (board[r][c] == '.') continue;
-				maxR = Math.max(maxR, r);
-				minR = Math.min(minR, r);
-				maxC = Math.max(maxC, c);
-				minC = Math.min(minC, c);
+				if (input.charAt(c) == '.') continue;
+				if (maxR < r) maxR = r;
+				if (minR > r) minR = r;
+				if (maxC < c) maxC = c;
+				if (minC > c) minC = c;
 			}
 		}
-		if (maxR == minR && maxC == minC) {
-			System.out.println("0");
-			return;
-		}
-		for (int d = 0; d < 4; d++) {
-			play(maxR, maxC, minR, minC, d);
-		}
-
-		System.out.println(min);
-	}
-	static void play(int maxR, int maxC, int minR, int minC, int d){
-		int cnt = 0;
-		int targetR = (d / 2) * (N - 1);
-		int targetC = (d % 2) * (N - 1);
-		cnt += moveCount(maxR, minR, targetR);
-		cnt += moveCount(maxC, minC, targetC);
-		min = Math.min(min, cnt);
-	}
-
-	static int moveCount(int max, int min, int target) {
-		if (max == min) return 0;
-		int maxMove = Math.abs(target - max);
-		int minMove = Math.abs(target - min);
-		return Math.max(maxMove, minMove);
+		int moveLeftOrRight = minC == maxC ? 0 : Math.min(N - 1 - minC, maxC);
+		int moveTopOrBottom = maxR == minR ? 0 : Math.min(N - 1 - minR, maxR);
+		System.out.println(moveLeftOrRight + moveTopOrBottom);
 	}
 }
